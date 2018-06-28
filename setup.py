e193cca736a91c992d5e796d754b69d7357be905
Jozef Cechovsky <cechovsky.jozef@gmail.com>
2018-06-28 15:39:11 +0200

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 requirements = ['requests']
 
 setup(name='sygicmaps',
-      version='0.2.7-dev',
+      version='0.2.8-dev',
       description='Python client library Sygic maps services.',
       scripts=[],
       url='https://github.com/Sygic/sygic-maps-services-python',
--- sygicmaps/__init__.py
+++ sygicmaps/__init__.py
@@ -1,4 +1,4 @@
 from sygicmaps.client import Client
 from sygicmaps.input import Input
 
-__version__ = "0.2.7-dev"
+__version__ = "0.2.8-dev"
