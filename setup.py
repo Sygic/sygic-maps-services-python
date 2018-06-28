3a2971dbfe0f68d6d2908478247d92367f4f531d
Jozef Cechovsky <cechovsky.jozef@gmail.com>
2018-06-28 15:31:58 +0200

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 requirements = ['requests']
 
 setup(name='sygicmaps',
-      version='0.2.6-dev',
+      version='0.2.7-dev',
       description='Python client library Sygic maps services.',
       scripts=[],
       url='https://github.com/Sygic/sygic-maps-services-python',
